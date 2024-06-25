4373177e7ea4a363fbc5937eda2ea7837b3af90b
Freddy Coaricona <fcoaricona@outlook.com>
2024-06-24 20:43:33 -0400

    feat: add gsap and ui hero

--- ui/hero.tsx
+++ ui/hero.tsx
@@ -1,5 +1,68 @@
+"use client";
+import { Suspense, useEffect, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+type VideoSource = string;
+const heroVideo = "/assets/videos/hero.mp4";
+const smallHeroVideo = "/assets/videos/smallHero.mp4";
+
 export default function Hero() {
+  const [videoSrc, setVideoSrc] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const handleVideoSrcSet = () => {
+      if (typeof window !== 'undefined') {
+        if (window.innerWidth < 760) {
+          setVideoSrc(smallHeroVideo);
+        } else {
+          setVideoSrc(heroVideo);
+        }
+      }
+    };
+    handleVideoSrcSet();
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window?.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
+  }, []);
+
   return (
-    <div>Hero</div>
-  )
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <Suspense fallback={<p>hola</p>}>
+            <video
+              className="pointer-events-none"
+              autoPlay
+              muted
+              playsInline={true}
+              key={videoSrc}
+            >
+              <source src={videoSrc} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </Suspense>
+        </div>
+      </div>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  );
 }
