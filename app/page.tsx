9120ecc8679d838c56f594be4521ae42d9950378
Freddy Coaricona <fcoaricona@outlook.com>
2024-07-05 19:48:19 -0400

    feat: UI pruebaGLB modify by cubo

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/ui/hero";
 import Navbar from "@/ui/navbar";
 import Highlights from "@/ui/highlights";
 import Model from "@/ui/model";
+import PruebaGLB from "@/ui/pruebaGLB";
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       <Hero />
       <Highlights />
       <Model />
+      <div style={{ height: "100vh" }}>
+        <PruebaGLB />
+      </div>
     </main>
   );
 }
--- ui/pruebaGLB.tsx
+++ ui/pruebaGLB.tsx
@@ -1,5 +1,45 @@
-export default function pruebaGLB() {
+"use client";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { Mesh } from "three";
+
+const Box: React.FC<any> = (props) => {
+  const meshRef = useRef<Mesh>();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+    }
+  });
+
   return (
-    <div>pruebaGLB</div>
-  )
-}
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
+const PruebaGLB: React.FC = () => {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[0, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default PruebaGLB;
