ada09210d396f1976959edfe7d95c8f64853c752
Freddy Coaricona <fcoaricona@outlook.com>
2024-07-09 01:03:16 -0400

    chore: perform massive file cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 import Hero from "@/ui/hero";
 import Navbar from "@/ui/navbar";
 import Highlights from "@/ui/highlights";
-import Model from "@/ui/model";
-import PruebaGLB from "@/ui/pruebaGLB";
-import ModelosGLTF from "@/ui/modelosGLTF";
-import ModelViewV2 from "@/ui/ModelViewV2";
-import InteractiveIphone from "@/ui/interactiveIphone";
 import ModelInteractive from "@/ui/modelInteractive";
 
 export default function Home() {
@@ -14,53 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Highlights />
-      {/* <Model /> */}
       <ModelInteractive />
-      {/* <div
-        className="screen-max-width"
-        style={{
-          height: "100vh",
-          width: "50vw",
-          border: "5px solid blue",
-          margin: "0 auto",
-        }}
-      >
-        <PruebaGLB />
-      </div>
-      <div
-        className="screen-max-width"
-        style={{
-          height: "100vh",
-          width: "50vw",
-          border: "5px solid blue",
-          margin: "0 auto",
-        }}
-      >
-        <ModelosGLTF />
-      </div>
-      <div
-        className="screen-max-width"
-        style={{
-          height: "100vh",
-          width: "50vw",
-          border: "5px solid blue",
-          margin: "0 auto",
-        }}
-      >
-        <ModelViewV2 />
-      </div>
-      <div
-        className="screen-max-width"
-        style={{
-          height: "100vh",
-          width: "50vw",
-          border: "5px solid blue",
-          margin: "0 auto",
-        }}
-      >
-        <InteractiveIphone />
-      </div> */}
-      
     </main>
   );
 }
--- ui/sceneGLB.tsx
+++
@@ -1,5 +0,0 @@
-export default function sceneGLB() {
-  return (
-    <div>sceneGLB</div>
-  )
-}
